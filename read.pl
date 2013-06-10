6d4d3008de4fa75f50582de2d34cb2a1404996a4
laphroaig <laphroaig@server.fake>
2013-06-09 20:35:36 -0700

    Redid filter config.

--- read.pl
+++ read.pl
@@ -2,9 +2,20 @@
 use strict;
 use warnings;
 
+# Constansts
+
+my $TIME_HOUR = 3600;
+my $TIME_WORK_DAY = 8 * $TIME_HOUR;
+
 # Config data
 my $DATA_DIR = '.';
 my $LOG_FILE = 'time_log';
+
+my %AN_CONF = {
+    anSkippProject => {
+        skippChar => '_',
+    },
+};
 
 # Config keys
 my $CK_LOG_PATH = "log_file";
@@ -12,7 +23,12 @@ my $CK_DATA_DIR = "data_dir";
 
 # Local data
 my @TIME_DATA;
+my @TIME_DATA_hash;
 my %CONFIG;
+my @ANALYSIS_FUNCTOPNS = (
+        \&anSkippProject,
+        \&anLongEntry,
+    );
 
 sub initData {
     die "Data dir don't exists: $DATA_DIR\n" unless -d $DATA_DIR;
@@ -36,6 +52,10 @@ sub readLog {
             if ($lastProject) {
                 $time = $1 - $lastTimestamp;
                 push @TIME_DATA, [$lastProject, $1 - $lastTimestamp, $lastTimestamp];
+                push @TIME_DATA_hash, {
+                    project => $lastProject, 
+                    timeDiff => $1 - $lastTimestamp, 
+                    timeStamp => $lastTimestamp};
             }
             $lastProject = $2;
             $lastTimestamp = $1;
@@ -68,31 +88,46 @@ sub analyse_data {
     my $i;
     my %hash;
 
-    foreach $i (0..@TIME_DATA-1) {
-        my $project = $TIME_DATA[$i][0];
-        my $time = $TIME_DATA[$i][1];
-        my $start_time = $TIME_DATA[$i][2];
-        
-        my $skipp;
-        if ($project =~ /^_/) {
-            #print "Skipping :" . $project . "\n";
-            $skipp = "y";
-        } elsif ($time > (8 * 3600)) {
-            my @timeArr = getHourAndMin($time);
-            print "Time entry for project $project is over 8h. " .
-                "($timeArr[0]h $timeArr[1]min)\n";
-            print "Skipp entry? ";
-            my $i = ask("(Y/n)");
-            if ($i =~ "y") {
-                $skipp = "y";
+    foreach my $entry (@TIME_DATA_hash) {
+        my $skipp = 'n';
+        foreach my $anFunc (@ANALYSIS_FUNCTOPNS) {
+            if (not $skipp =~ /y/i) {
+                $skipp = &{$anFunc}($entry);
             }
+            #$anFunc->();
         }
 
-        if (not $skipp) {
-            $hash{$project} += $time;
+        if (not $skipp =~ /y/i) {
+            $hash{$entry->{project}} += $entry->{timeDiff};
             #print "Adding $time to $project total is $hash{$project}\n";
         }
     }
+
+#    foreach $i (0..@TIME_DATA-1) {
+#        my $project = $TIME_DATA[$i][0];
+#        my $time = $TIME_DATA[$i][1];
+#        my $start_time = $TIME_DATA[$i][2];
+#        
+#        my $skipp;
+#        if ($project =~ /^_/) {
+#            #print "Skipping :" . $project . "\n";
+#            $skipp = "y";
+#        } elsif ($time > (8 * 3600)) {
+#            my @timeArr = getHourAndMin($time);
+#            print "Time entry for project $project is over 8h. " .
+#                "($timeArr[0]h $timeArr[1]min)\n";
+#            print "Skipp entry? ";
+#            my $i = ask("(Y/n)");
+#            if ($i =~ "y") {
+#                $skipp = "y";
+#            }
+#        }
+
+#        if (not $skipp) {
+#            $hash{$project} += $time;
+#            #print "Adding $time to $project total is $hash{$project}\n";
+#        }
+#    }
 
     %hash;
 }
@@ -100,8 +135,8 @@ sub analyse_data {
 sub getHourAndMin {
     my $time = shift;
 
-    my $hour = int($time / 3600);
-    my $min = int(($time - ($hour * 3600)) / 60);
+    my $hour = int($time / $TIME_HOUR);
+    my $min = int(($time - ($hour * $TIME_HOUR)) / 60);
 
     ($hour, $min);
 }
@@ -126,6 +161,39 @@ sub debugPrintLog {
         print "\n";
     }
     print "-----------\n";
+
+    for my $entry (@TIME_DATA_hash) {
+        print $entry->{project} . ' ';
+        print $entry->{timeDiff} . ' ';
+        print $entry->{timeStamp}. "\n";
+    }
+}
+
+sub anLongEntry {
+    my $entry = shift;
+
+    my $q = 'n';
+    if ($entry->{timeDiff} > $TIME_WORK_DAY) {
+        my @timearr = getHourAndMin($entry->{timeDiff});
+        print "time entry for project " .$entry->{project} . " is over 8h. " .
+            "($timearr[0]h $timearr[1]min)\n";
+        print "skipp entry? ";
+        my $q = ask("(Y/n)");
+    }
+
+    return lc $q;
+}
+
+sub anSkippProject {
+    my $entry = shift;
+
+    my $skippChar = $AN_CONF{anSkippProject}->{skippChar};
+    print "skippChar $skippChar\n". keys (&$AN_CONF{anSkippProject}) . "\n";
+    if ($entry->{project} =~ /$skippChar/) {
+        return 'y';
+    } else {
+        return 'n';
+    }
 }
 
 initData();
